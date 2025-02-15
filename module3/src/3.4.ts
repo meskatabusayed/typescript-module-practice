56bb2b0f3bf1102ae4c32848f802c1cf66ae0e5b
meskatabusayed <meskatabusayed@gmail.com>
2025-02-15 16:55:06 +0530

    instanceof type gurd p-2

--- module3/src/3.4.ts
+++ module3/src/3.4.ts
@@ -30,7 +30,29 @@
         }
     }
 
-    const cat = new Cat("skdj" , "sewe");
-    cat.makeMeaw()
-   
+    //smart way
+
+    const isDog = (animal : Animal) : animal is Dog => {
+        return animal instanceof Dog;
+    }
+
+    const isCat = (animal : Animal) : animal is Cat => {
+        return animal instanceof Cat;
+    }
+
+    const getAnimal = (animal : Animal) => {
+        if(isDog(animal)){
+            animal.makeBark();
+        }
+        else if(isCat(animal)){
+            animal.makeMeaw();
+        }
+        else{
+            animal.makeSound();
+        }
+    }
+
+    const dog = new Dog("dewr" , "wedwe");
+    const cat = new Cat("dewrC" , "wedweC2");
+    getAnimal(dog);
 }
