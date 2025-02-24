b8c2976fdc22be8f8de32f7a30f8978d5d7821d1
meskatabusayed <meskatabusayed@gmail.com>
2025-02-25 01:04:15 +0530

    conditional types added

--- module2/src/2.9.ts
+++ module2/src/2.9.ts
@@ -1,5 +1,22 @@
 {
-    //conditional types
+    //conditional types : typescript a jodi kono type onno kono akti type ar oporee conditionaly dependent thake tahole seti hobe conditional type
+    //1
+    type a1 = number;
+    type b1 = string;
+    type x = a1 extends null ? true : false;
+    type y = a1 extends null ? true : b1 extends undefined ? undefined : any;
+      //2
+      type Sheikh = {
+        bike : string;
+        car : string;
+        ship : string;
+        plane : string;
+      }
+
+      type CheckVehicle<T> = T extends keyof Sheikh ? true : false;
+      type HasPlane = CheckVehicle<"plane">
+
+
 
     
 }
