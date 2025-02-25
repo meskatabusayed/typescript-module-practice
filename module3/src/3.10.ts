d9a769c020a4a543f17c330d1851409dbbc05b10
meskatabusayed <meskatabusayed@gmail.com>
2025-02-25 22:47:22 +0530

    encapsulation

---
+++ module3/src/3.10.ts
@@ -0,0 +1,29 @@
+{
+
+    class BankAccount {
+        public readonly id : number;
+        public name : string;
+        protected _balance : number;
+        constructor(id : number , name : string , balance : number){
+            this.id = id;
+            this.name = name;
+            this._balance = balance;
+        }
+        addDeposit(amount : number) {
+            return this._balance = this._balance + amount;
+        }
+    
+         private getBalance(){
+            return this._balance;
+        }
+      }
+
+      class StudentAccount extends BankAccount{
+        test(){
+            this.id
+        }
+      }
+
+      
+    
+}
