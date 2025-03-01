d1710000424e64646517884047f2dbba698e8c0f
meskatabusayed <meskatabusayed@gmail.com>
2025-03-01 22:56:11 +0530

    problem added and solve it

--- problem_solving/src/oop.ts
+++ problem_solving/src/oop.ts
@@ -56,9 +56,83 @@ details. */
     }
   }
 
-  const bankAccount = new BankAccount(100);
-  bankAccount.deposit(100);
-  bankAccount.withdrawal(20);
-  bankAccount.withdrawal(20);
-  bankAccount.withdrawal(20);
+  // const bankAccount = new BankAccount(100);
+  // bankAccount.deposit(100);
+  // bankAccount.withdrawal(20);
+  // bankAccount.withdrawal(20);
+  // bankAccount.withdrawal(20);
+
+
+  /*  3. Define a Student class with name, age, and grade properties. Add a method to display details. */
+
+  class Student{
+    constructor(public name : string, public age : number, public grade : string){}
+    displayDetails(){
+      console.log(`Name : ${this.name} , Age : ${this.age} and Grade : ${this.grade}`)
+    }
+  }
+
+  const student = new Student("Meskat" , 99 , "A+");
+  // student.displayDetails();
+
+  /* 4. Create a Rectangle class with properties width and height, and a method to calculate the area. */
+
+  class Rectangle{
+    constructor(public width : number , public height : number){}
+    calculateArea(){
+      if(typeof(this.height) === "number" && typeof(this.width) === "number"){
+        console.log(this.height * this.width)
+      }else{
+        console.log("Incorrect Input")
+      }
+    }
+  }
+
+  const rectangle = new Rectangle(459 , 78);
+  // rectangle.calculateArea()
+
+  /*  5. Implement a Circle class with a method to calculate the circumference 
+  Circumference(পরিধি)=2πr
+  */
+
+  class Circle{
+    calculateCircumference(radius : number) : number{
+      return (2*3.1416*radius);
+
+
+    }
+  }
+
+  const circle = new Circle();
+  // console.log(circle.calculateCircumference(2));
+
+  /*  6. Make the balance property of BankAccount private and provide getBalance() and setBalance(). */
+
+  class BankAccount2{
+    private _balance : number;
+    constructor(public initialBalance : number){
+      this._balance = initialBalance;
+    }
+    getBalance(){
+      console.log(`Balance : ${this._balance}`)
+    }
+
+    setBalance(amount : number) : void{
+      if(amount > 0){
+        this._balance += amount;
+        console.log(`Deposit Amount : ${amount}. New Balance : ${this._balance}`)
+      }else{
+        console.log("Invalid Deposit Amount")
+      }
+
+    }
+
+  }
+
+  const bankAccount2 = new BankAccount2(100)
+  bankAccount2.getBalance();
+  bankAccount2.setBalance(2000);
+  bankAccount2.setBalance(5500);
+
+
 }
